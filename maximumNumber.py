62b69fe475aebf4e5bf9928b3ea2103b333bdb4c
ritzzi23 <ojharitesh1998@gmail.com>
2018-06-01 12:03:46 +0530

    comments added

--- maximumNumber.py
+++ maximumNumber.py
@@ -1,8 +1,11 @@
+#Enter three numbers 
 x= int(input ("Please enter 1st number: "))
 y= int(input ("Please enter 2nd number: "))
 z= int(input ("Please enter 3rd number: "))
 
+#maximum of three numbers will be displayed
 print("Maximum of three values is:")
 print(max(x,y,z))
- 
+
+#This is done to stop the console window
 input("Please press enter to exit")
